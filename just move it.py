cd9b0b50d64556c00cdd459b713c247767e79217
LukasNiu <71536446+LukasNiu@users.noreply.github.com>
2020-09-19 10:31:21 +0800

    Add files via upload

---
+++ just move it.py
@@ -0,0 +1,14 @@
+a_1 = input("a=")
+b_1 = input("b=")
+c_1 = input("c=")
+
+
+def function (a,b,c):
+    print("y=" + str(a) +"(x+ "+ str(b/(a*2)) + ")+ " + str(4*a*c-b*b/(4*a)))
+    return
+function(int(a_1),int(b_1),int(c_1))
+
+
+
+
+
